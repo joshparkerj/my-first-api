56a402c9003a6c785fcfca35112bddd8a20590a9
joshparkerj <superfakek@gmail.com>
2018-09-13 18:47:21 -0600

    install npm packages express and body-parser

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log('node js ran this code');
+
+// let's install some helpful packages
+
+// express and body-parser
+
+// express is a very helpful package for running http servers
+
+// body-parser is a package for parsing http requests
+
+// to sound smart in interviews, say this.
+
+// node just runs javascript
+
+// express is a node framework for writing server code
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
